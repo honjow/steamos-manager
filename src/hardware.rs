040e64b786b67385ff417e095fb5a80f1fbde87c
honjow <honjow311@gmail.com>
2025-04-23 03:27:39 +0800

    hardware: Update device variant to return Generic for unknown types

--- src/hardware.rs
+++ src/hardware.rs
@@ -83,7 +83,7 @@ pub(crate) async fn device_variant() -> Result<(DeviceType, String)> {
             (DeviceType::LegionGoS, product_name.to_string())
         }
         ("Valve", _, "Jupiter" | "Galileo") => (DeviceType::SteamDeck, board_name.to_string()),
-        _ => (DeviceType::Unknown, String::from("unknown")),
+        _ => (DeviceType::Generic, String::from("generic")),
     })
 }
 
