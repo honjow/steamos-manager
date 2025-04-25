4180ca49ce3c37cc96e62a6c91ec74edaab7525a
Matthew Schwartz <matthew.schwartz@linux.dev>
2025-04-24 21:34:56 -0700

    hardware/platform: Add hardware detection for ROG Ally and ROG Ally X

--- src/platform.rs
+++ src/platform.rs
@@ -157,6 +157,7 @@ impl PlatformConfig {
         let platform = match device_type().await? {
             DeviceType::SteamDeck => "jupiter",
             DeviceType::LegionGoS => "legion-go-s",
+            DeviceType::Ally => "ally",
             _ => return Ok(None),
         };
         let config = read_to_string(format!(
